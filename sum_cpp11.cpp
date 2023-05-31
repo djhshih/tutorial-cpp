3b1e831f1953222eb171b80cf5c4417b36a4a9e1
David J. H. Shih <djh.shih@gmail.com>
2023-05-31 16:37:11 +0800

    Use auto type inference (C++11).

--- sum_cpp11.cpp
+++ sum_cpp11.cpp
@@ -12,17 +12,15 @@ int main(int argc, char** argv) {
 	// ignore first argument
 	size_t n = argc - 1;
 	std::vector<double> numbers(n);
-	std::vector<double>::iterator it = numbers.begin();
 	char** argv_it = &(argv[1]);
-	for (it = numbers.begin(); it != numbers.end(); ++it, ++argv_it) {
+	for (auto it = numbers.begin(); it != numbers.end(); ++it, ++argv_it) {
 		std::string s = (*argv_it);
 		*it = std::stod(s);
 	}
 
 	// sum numbers together
 	double sum_value = 0.0;
-	std::vector<double>::const_iterator cit;
-	for (cit = numbers.cbegin(); cit != numbers.cend(); ++cit) {
+	for (auto cit = numbers.cbegin(); cit != numbers.cend(); ++cit) {
 		sum_value += (*cit);
 	}
 
